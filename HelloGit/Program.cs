d9ce26f3e4cf0b4ade6120d5f7c4320f01af902b
Valeria <lera14779@gmail.com>
2024-10-16 18:35:29 +0400

    Added message from the first participant

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Git! Welcome to Visual Studio!");
+            Console.WriteLine("Hello, Git! This is the first participant.");
         }
     }
 }
